29e1c51d4d16b8f9958bac921dfbd88e9ae3ba49
Filipe Calo <filipe.calo>
2020-11-23 19:58:16 -0300

    add new func Alive in controller and expose a new port in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM golang:1.15.5-alpine3.12
 
-COPY . "$GOPATH"/app
-
-WORKDIR "$GOPATH"/app
+COPY . .
 
 RUN apk update && \
     apk add --no-cache bash=5.0.17-r0 git=2.26.2-r0 openssh=8.3_p1-r0 curl=7.69.1-r1 && \
-    chmod +x "$GOPATH"/app/deps.sh && \
-    sh /"$GOPATH"/app/deps.sh && \ 
-    go build "$GOPATH"/app/main.go
+    chmod +x deps.sh && \
+    sh deps.sh
+    
+EXPOSE 8001
 
-CMD ["$GOPATH/app/sms"]
+
+ENTRYPOINT [ "go","run","main.go" ]
--- main.go
+++ main.go
@@ -6,15 +6,22 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/fmendonca/sms/controllers"
 	"github.com/fmendonca/sms/routes"
 )
 
 func main() {
 
 	port := map[bool]string{true: os.Getenv("PORT"), false: "8000"}[os.Getenv("PORT") != ""]
-
+	apiAlive := http.NewServeMux()
+	apiAlive.HandleFunc("/api/health", controllers.Alive)
 	routes.Rotas()
 	log.Println("Server is listening at", port)
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 
+	go func() {
+		log.Println("Server is listening at 8001 /api/health")
+		http.ListenAndServe("localhost:8001", apiAlive)
+	}()
+
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
